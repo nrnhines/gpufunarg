28001f8d9911c8e841c2a3af1f5ad8ac64f04273
Michael Hines <michael.hines@yale.edu>
2016-08-16 20:38:19 -0400

    wrap the function pointer in a structure

--- main.c
+++ main.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
-typedef int(*F)(int);
+typedef struct {
+  int(*f)(int);
+} F;
+
 extern int a(int, F);
 extern int b(int);
 
 int main() {
   int x;
-  x = a(1000, b);
+  F f;
+  f.f = b;
+
+  x = a(1000, f);
   printf("x=%d\n", x);
   return 0;
 }
@@ -15,7 +21,7 @@ int a(int n, F f) {
   int i, x;
   x = 0;
   for (i = 0; i < n ; ++i) {
-    x += (*f)(i);
+    x += (*(f.f))(i);
   }
   return x;
 }
