c40408c1eb8cf558bb48f284c7f6cdb5251df7ba
Michael Hines <michael.hines@yale.edu>
2016-08-17 09:46:18 -0400

    function pointers are unsupported at present. Only way is to have if else or switch using a compile time list of desired functions to call.

--- main.c
+++ main.c
@@ -1,31 +1,61 @@
 #include <stdio.h>
 
-typedef struct {
-  int(*f)(int);
-} F;
+extern int a(int, int);
 
-extern int a(int, F);
+int steer(int i, int ifun);
+#pragma acc routine(steer) seq
+
 extern int b(int);
+#pragma acc routine(b) seq
+
+extern int c(int);
+#pragma acc routine(c) seq
 
 int main() {
   int x;
-  F f;
-  f.f = b;
 
-  x = a(1000, f);
+  x = a(1000, 1);
   printf("x=%d\n", x);
+
+  x = a(1000, 2);
+  printf("x=%d\n", x);
+
   return 0;
 }
 
-int a(int n, F f) {
+int a(int n, int ifun) {
+
   int i, x;
   x = 0;
-  for (i = 0; i < n ; ++i) {
-    x += (*(f.f))(i);
+  #pragma acc parallel loop
+  for (int i = 0; i < n ; ++i) {
+    #pragma acc atomic update
+    x += steer(i, ifun);
   }
   return x;
 }
 
 int b(int i) {
   return 2*i;
+}
+
+int c(int i) {
+  return 3*i;
+}
+
+int steer(int i, int ifun) {
+  int x;
+#if 1
+  switch (ifun) {
+    case 1: x = b(i); break;
+    case 2: x = c(i); break;
+  }
+#else
+  if (ifun == 1) {
+    x = b(i);
+  }else{
+    x = c(i);
+  }
+#endif
+  return x;
 }
